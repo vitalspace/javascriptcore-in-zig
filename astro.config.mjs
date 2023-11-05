9938fac0ab16c996f6790d17fd4efb977060c8cf
vitalspace <myglobalsounds@gmail.com>
2023-11-04 21:37:16 -0600

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://vitalspace.github.io/javascriptcore-in-zig/',
+  site: 'https://vitalspace.github.io',
   base: '/',
 });
