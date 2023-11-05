fa3f1e047e34654d0df7b502e34c62d8d9622829
vitalspace <myglobalsounds@gmail.com>
2023-11-04 21:48:22 -0600

    added url base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://vitalspace.github.io/',
-  base: '/',
+  base: '/javascriptcore-in-zig',
 });
